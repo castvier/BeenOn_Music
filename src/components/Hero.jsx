9dfd66dfd4aaf2d52adae2baa094c70419b140ae
Jimwell Castillo <castvier@gmail.com>
2024-12-08 13:22:37 -0800

    Update background image to use external URL with new styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,8 +25,15 @@ const BackgroundImage = styled.div`
   background-image: url('https://i.ibb.co/hyp9m8T/hero-bg.png');
   background-size: cover;
   background-position: center;
-  opacity: 0.5;
-  z-index: 1;
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.4);
+  }
 `;
 
 const Content = styled(motion.div)`
